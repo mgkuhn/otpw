23f76eacfe5eab74002745f50e607223a5c7193c
Markus Kuhn <Markus.Kuhn@cl.cam.ac.uk>
2003-06-19 20:09:22 +0000

    added comments

--- otpw-l.c
+++ otpw-l.c
@@ -1,3 +1,17 @@
+/*
+ * One-time password login capability
+ *
+ * This file is just a variant of otpw.c, in which the debugging
+ * messages are directed to the log_message function of pam_otpw.c
+ * instead of to stderr. Link otpw-l.o instead of otpw.o into
+ * pam_otpw.so for debugging purposes.
+ *
+ * Markus Kuhn <http://www.cl.cam.ac.uk/~mgk25/>
+ *
+ * $Id: otpw-l.c,v 1.2 2003-06-19 20:09:22 mgk25 Exp $
+ */
+
+
 #include <syslog.h>
 
 extern void log_message(int priority, void *pamh,
